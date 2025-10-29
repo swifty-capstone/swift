6ec260667cb75330ff1be43bff903f4c7c060817
seungju08 <cntmdwn5522@gmail.com>
2025-10-29 16:03:14 +0900

    Initial Commit

---
+++ Gyeongso-Out/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gyeongso-Out
+//
+//  Created by 추승주 on 10/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gyeongso-Out/Gyeongso_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gyeongso_OutApp.swift
+//  Gyeongso-Out
+//
+//  Created by 추승주 on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Gyeongso_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
