cde57cdf9aa3a16e5e908bcfc66bc219f2b351bf
seungju08 <cntmdwn5522@gmail.com>
2025-10-29 16:23:56 +0900

    SignInView 파일 생성[20251029]

---
+++ Gyeongso-Out/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Gyeongso-Out
+//
+//  Created by 추승주 on 10/29/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("재한 ㅎㅇ~")
+    }
+}
+
+#Preview {
+    SignInView()
+}
